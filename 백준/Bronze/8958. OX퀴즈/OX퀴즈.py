3cb41d1defc7ae30d65c634ce78fc81d4cb28d6f
eehyo <112912122+eehyo@users.noreply.github.com>
2022-11-28 14:34:12 +0900

    [Bronze II] Title: OX퀴즈, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -3,11 +3,11 @@ N=int(input())
 for i in range(N):
     a = input()
     score = 0
-    sum = 0
+    total = 0
     for j in a:
         if j == 'O':
             score += 1
         else:
             score = 0
-        sum += score
-    print(sum)
+        total += score
+    print(total)
